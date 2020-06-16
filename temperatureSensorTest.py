988a8ce04151c67645d3c4650712eabd41c7824f
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-06-16 17:02:33 -0600

    Update temperatureSensorTest.py

--- temperatureSensorTest.py
+++ temperatureSensorTest.py
@@ -5,8 +5,8 @@ spi0.open(0, 0)
 spi1 = spidev.SpiDev()
 spi1.open(0, 1)
 
-temp0_raw = spi0.readbytes(13)  #Read just the first 13 bits, as thelast three bits of the 16 byte word are not used
-temp1_raw = spi1.readbytes(13)  
+temp0_raw = spi0.readbytes(1)  #Read just the first 13 bits, as thelast three bits of the 16 byte word are not used
+temp1_raw = spi1.readbytes(1)  
 
 print("Raw data temp0: ")
 print(temp0_raw)
@@ -14,8 +14,8 @@ print(temp0_raw)
 print("Raw data temp1: ")
 print(temp1_raw)
 
-temp0 = temp0_raw * 0.0625
-temp1 = temp1_raw * 0.0625
+temp0 = temp0_raw[0] * 0.0625
+temp1 = temp1_raw[0] * 0.0625
 
 print("temp0: ")
 print(temp0)
