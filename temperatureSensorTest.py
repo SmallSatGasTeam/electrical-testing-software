2664a196b92dd6fe48df6697ef6809cab59fd018
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-06-20 14:50:11 -0600

    Update temperatureSensorTest.py

--- temperatureSensorTest.py
+++ temperatureSensorTest.py
@@ -16,8 +16,8 @@ print(temp1_raw)
 
 #This function converts the two bytes to the temperature value. The data is stored as two bytes where T13 is the most significant bit
 #| T13 T12 T11 T10 T9 T8 T7 T6 |  | T5 T4 T3 T2 T1 1 1 1 |
-temp0 = ((temp0_raw[0] * 512) + (temp0_raw[1] - 7))* 0.0625
-temp1 = ((temp1_raw[0] * 512) + (temp1_raw[1] - 7))* 0.0625
+temp0 = ((temp0_raw[0] * 64) + (temp0_raw[1] >> 3))* 0.0625
+temp1 = ((temp1_raw[0] * 64) + (temp1_raw[1] >> 3))* 0.0625
 
 print("temp0: ")
 print(temp0)
