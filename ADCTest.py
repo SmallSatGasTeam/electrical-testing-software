1ecd95f89fde649835dd03936379b54a6d55114c
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-07-11 15:28:36 -0600

    Corrected channel select bytes

--- ADCTest.py
+++ ADCTest.py
@@ -2,30 +2,23 @@ import spidev
 import RPi.GPIO as GPIO
 
 adc_cs = 22
-sclk = 23
-miso = 21
-mosi = 19
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(adc_cs, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(sclk, GPIO.OUT)
-GPIO.setup(miso, GPIO.IN)
-GPIO.setup(mosi, GPIO.OUT)
 
 input("Connecting to ADC, press <Enter> to continue...")
 
 spi = spidev.SpiDev()
 spi.open(0,0)
 spi.no_cs = True
-GPIO.output(adc_cs, GPIO.LOW)
-GPIO.output(sclk, GPIO.LOW)
+GPIO.output(adc_cs, GPIO.HIGH)
 
 input("Getting data from UV sensor, channel 1, press <Enter> to continue...")
 
-msg = 0b11
-msg = ((msg << 1) + 1) << 5
-msg = [msg, 0b00000000]
+GPIO.output(adc_cs, GPIO.LOW)
+msg = [0b00001000, 0b00000000]
 uv_sense = spi.xfer2(msg)
+GPIO.output(adc_cs, GPIO.HIGH)
 uv_val = (uv_sense[0] + (uv_sense[1] * 512))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("UV sensor value: ")
 print(uv_sense)
@@ -33,10 +26,10 @@ print(uv_val)
 
 input("Getting data from Sun sensor 1, channel 5, press <Enter> to continue...")
 
-msg = 0b11
-msg = ((msg << 1) + 5) << 5
-msg = [msg, 0b00000000]
+GPIO.output(adc_cs, GPIO.LOW)
+msg = [0b00101000, 0b00000000]
 sun1 = spi.xfer2(msg)
+GPIO.output(adc_cs, GPIO.HIGH)
 sun1_val = (sun1[0] + (sun1[1] * 512))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("Sun sensor 1 value: ")
 print(sun1)
@@ -44,10 +37,10 @@ print(sun1_val)
 
 input("Getting data from Sun sensor 2, channel 4, press <Enter> to continue...")
 
-msg = 0b11
-msg = ((msg << 1) + 4) << 5
-msg = [msg, 0b00000000]
+GPIO.output(adc_cs, GPIO.LOW)
+msg = [0b00100000, 0b00000000]
 sun2 = spi.xfer2(msg)
+GPIO.output(adc_cs, GPIO.HIGH)
 sun2_val = (sun2[0] + (sun2[1] * 512))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("Sun sensor 2 value: ")
 print(sun2)
@@ -55,10 +48,10 @@ print(sun2_val)
 
 input("Getting data from Sun sensor 3, channel 2, press <Enter> to continue...")
 
-msg = 0b11
-msg = ((msg << 1) + 2) << 5
-msg = [msg, 0b00000000]
+GPIO.output(adc_cs, GPIO.LOW)
+msg = [0b00010000, 0b00000000]
 sun3 = spi.xfer2(msg)
+GPIO.output(adc_cs, GPIO.HIGH)
 sun3_val = (sun3[0] + (sun3[1] * 512))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("Sun sensor 3 value: ")
 print(sun3)
@@ -66,10 +59,10 @@ print(sun3_val)
 
 input("Getting data from Sun sensor 4, channel 3, press <Enter> to continue...")
 
-msg = 0b11
-msg = ((msg << 1) + 3) << 5
-msg = [msg, 0b00000000]
+GPIO.output(adc_cs, GPIO.LOW)
+msg = [0b00011000, 0b00000000]
 sun4 = spi.xfer2(msg)
+GPIO.output(adc_cs, GPIO.HIGH)
 sun4_val = (sun4[0] + (sun4[1] * 512))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("Sun sensor 4 value: ")
 print(sun4)
@@ -77,18 +70,14 @@ print(sun4_val)
 
 input("Getting data from Sun sensor 5, channel 0, press <Enter> to continue...")
 
-msg = 0b11
-msg = ((msg << 1) + 0) << 5
-msg = [msg, 0b00000000]
+GPIO.output(adc_cs, GPIO.LOW)
+msg = [0b00000000, 0b00000000]
 sun5 = spi.xfer2(msg)
+GPIO.output(adc_cs, GPIO.HIGH)
 sun5_val = (sun5[0] + (sun5[1] * 512))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("Sun sensor 5 value: ")
 print(sun5)
 print(sun5_val)
-
-
-GPIO.output(adc_cs, GPIO.HIGH)
-GPIO.output(sclk, GPIO.HIGH)
 
 spi.close()
 GPIO.cleanup()
