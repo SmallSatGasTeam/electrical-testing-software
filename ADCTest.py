480e5aed1cd395b2efaa4d03828805b412d7c1ce
Logan Freeman <lfreeman@gas.usu.edu>
2020-08-20 03:07:34 +0100

    This version marks the tested and functioning ADCTest code. The ADC_Callibrate file is also added and has been tested and verified

--- ADCTest.py
+++ ADCTest.py
@@ -10,6 +10,7 @@ input("Connecting to ADC, press <Enter> to continue...")
 
 spi = spidev.SpiDev()
 spi.open(0,0)
+spi.max_speed_hz = 1000
 spi.no_cs = True
 GPIO.output(adc_cs, GPIO.HIGH)
 
@@ -17,6 +18,7 @@ input("Getting data from UV sensor, channel 1, press <Enter> to continue...")
 
 GPIO.output(adc_cs, GPIO.LOW)
 msg = [0b00001000, 0b00000000]
+uv_sense = spi.xfer2(msg)
 uv_sense = spi.xfer2(msg)
 GPIO.output(adc_cs, GPIO.HIGH)
 uv_val = (uv_sense[1] + (uv_sense[0] * 256))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
@@ -29,6 +31,7 @@ input("Getting data from Sun sensor 1, channel 5, press <Enter> to continue...")
 GPIO.output(adc_cs, GPIO.LOW)
 msg = [0b00101000, 0b00000000]
 sun1 = spi.xfer2(msg)
+sun1 = spi.xfer2(msg)
 GPIO.output(adc_cs, GPIO.HIGH)
 sun1_val = (sun1[1] + (sun1[0] * 256))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("Sun sensor 1 value: ")
@@ -39,6 +42,7 @@ input("Getting data from Sun sensor 2, channel 4, press <Enter> to continue...")
 
 GPIO.output(adc_cs, GPIO.LOW)
 msg = [0b00100000, 0b00000000]
+sun2 = spi.xfer2(msg)
 sun2 = spi.xfer2(msg)
 GPIO.output(adc_cs, GPIO.HIGH)
 sun2_val = (sun2[1] + (sun2[0] * 256))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
@@ -51,6 +55,7 @@ input("Getting data from Sun sensor 3, channel 2, press <Enter> to continue...")
 GPIO.output(adc_cs, GPIO.LOW)
 msg = [0b00010000, 0b00000000]
 sun3 = spi.xfer2(msg)
+sun3 = spi.xfer2(msg)
 GPIO.output(adc_cs, GPIO.HIGH)
 sun3_val = (sun3[1] + (sun3[0] * 256))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("Sun sensor 3 value: ")
@@ -61,6 +66,7 @@ input("Getting data from Sun sensor 4, channel 3, press <Enter> to continue...")
 
 GPIO.output(adc_cs, GPIO.LOW)
 msg = [0b00011000, 0b00000000]
+sun4 = spi.xfer2(msg)
 sun4 = spi.xfer2(msg)
 GPIO.output(adc_cs, GPIO.HIGH)
 sun4_val = (sun4[1] + (sun4[0] * 256))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
@@ -73,6 +79,7 @@ input("Getting data from Sun sensor 5, channel 0, press <Enter> to continue...")
 GPIO.output(adc_cs, GPIO.LOW)
 msg = [0b00000000, 0b00000000]
 sun5 = spi.xfer2(msg)
+sun5 = spi.xfer2(msg)
 GPIO.output(adc_cs, GPIO.HIGH)
 sun5_val = (sun5[1] + (sun5[0] * 256))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
 print("Sun sensor 5 value: ")
@@ -81,3 +88,4 @@ print(sun5_val)
 
 spi.close()
 GPIO.cleanup()
+
---
+++ ADC_Callibrate.py
@@ -0,0 +1,91 @@
+import numpy as np
+import spidev
+import RPi.GPIO as GPIO
+
+adc_cs = 22
+spi = spidev.SpiDev()
+
+def calculate_regression(expected, measured, error):
+    if len(expected) == len(measured):
+        # Setup matrices
+        Y = np.array([expected])
+        Y = np.transpose(Y)
+        X = np.array([measured])
+        X = np.transpose(X)
+        X = np.concatenate( (np.ones_like(X),X), axis=1 )
+        
+        # Solve for the coefficients of the linear equation
+        B = np.matmul(np.transpose(X),X)
+        B = np.linalg.inv(B)
+        B = np.matmul(B,np.transpose(X))
+        B = np.matmul(B,Y)
+
+        # Return the coefficients and the error vector
+        e_array = np.add(Y, -np.matmul(X,B))
+        for i in e_array:
+            error.append(float(i))
+        return B
+
+    return 0
+    
+def getSample(channel):
+    GPIO.output(adc_cs, GPIO.LOW)
+    msg = [0b00001000, 0b00000000]
+    uv_sense = spi.xfer2(msg)
+    uv_sense = spi.xfer2(msg)
+    GPIO.output(adc_cs, GPIO.HIGH)
+    uv_val = (uv_sense[1] + (uv_sense[0] * 256))*(3.3/4096)   #Converts the 12 bit serial signal to the voltage
+    return uv_val
+
+def main():
+   
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(adc_cs, GPIO.OUT, initial=GPIO.HIGH)
+
+    # Setup ADC
+    input("Connecting to ADC, press <Enter> to continue...")
+
+    spi.open(0,0)
+    spi.max_speed_hz = 1000
+    spi.no_cs = True
+    GPIO.output(adc_cs, GPIO.HIGH)
+
+    # Samples need to be taken across the full range of the output up to 3V3
+    numSamples = int(input("Enter the number of samples to be taken per channel: "))
+    numChannels = int(input("Enter the number of channels to be calibrated: "))
+    expected = []
+    measured = []
+    
+    for chan in range(numChannels):
+        x = input("Connect voltage supply to channel {}".format(chan))
+        for voltage in range(numSamples):
+            x = input("Set supply to {} volts".format((voltage*3.3)/numSamples))
+            expected.append((voltage*3.3)/numSamples)
+            measured.append(getSample(chan))
+            
+
+    print(measured)
+    print(expected) 
+
+
+    reg_error = []
+    raw_error = []
+
+    #calculate raw error
+    for i in range(len(expected)):
+        raw_error.append(abs(expected[i] - measured[i]))
+
+    print('Maximum error before regression: {}'.format(max(raw_error)))
+
+    B = calculate_regression(expected, measured, reg_error)
+
+    print('Maximum error after regression: {}'.format(max(reg_error)))
+    print('Regression coefficients: ')
+    print(B)
+
+    spi.close()
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    main()
