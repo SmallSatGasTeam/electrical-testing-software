f69d925705f0a2997e77716b29519a3dd326a3be
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2021-05-13 10:41:55 -0600

    Create enableUHFTest.py

---
+++ enableUHFTest.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+
+UHFEn = 24
+
+GPIO.setup(UHFEn, GPIO.OUT, initial=GPIO.HIGH)
+print("Enabling UHF...")
+GPIO.output(UHFEn, GPIO.HIGH)
+input("UHF Enabled, Press enter to continue")
+print("Disabling UHF...")
+GPIO.output(EPSReset, GPIO.LOW)
+print("UHF Disabled, Press enter to continue")
+GPIO.output(UHFEn, GPIO.HIGH)
+GPIO.cleanup()
