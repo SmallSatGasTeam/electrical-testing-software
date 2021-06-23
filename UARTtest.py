269cc27724d737508fc1c7df1256635057f9cc6b
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2021-06-23 12:35:22 -0600

    Fixed serial port name

--- UARTtest.py
+++ UARTtest.py
@@ -1,6 +1,6 @@
 import serial
 
-ser = serial.Serial ("/dev/ttyAMA0")    #Open named port 
+ser = serial.Serial ("/dev/ttyS0")    #Open named port 
 ser.baudrate = 9600                     #Set baud rate to 9600
 data = "a"                              #Set data to the character 'a', 0x61 or 01100001
 ser.write(data)                         #Send the data
